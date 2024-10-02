988237bf247efac6035f3a08bd6010a0a22aa0a2
ProfessorLongBeard <austingarrick752@gmail.com>
2024-10-01 22:14:12 -0700

    message_box: Fix formatting of messages

--- src/message_box.c
+++ src/message_box.c
@@ -10,7 +10,8 @@ gint msgbox_info(const gchar *msg, ...) {
     gint ret = 0;
     va_list args;
     gchar *msg_tmp = NULL;
-    GtkWidget *msg_info = NULL;
+    GtkWidget *msg_info = NULL, *content = NULL, *label = NULL;
+
 
 
 
@@ -20,7 +21,7 @@ gint msgbox_info(const gchar *msg, ...) {
         return -1;
     }
 
-    msg_tmp = g_malloc(strlen(msg) + 1);
+    msg_tmp = g_malloc(strlen(msg) + 1024);
 
     if (!msg_tmp) {
         g_print("Failed to allocate info message buffer!\n");
@@ -28,17 +29,45 @@ gint msgbox_info(const gchar *msg, ...) {
     }
 
     va_start(args, msg);
-    ret = g_vsnprintf(msg_tmp, strlen(msg) + 1, msg, args);
+    ret = g_vsnprintf(msg_tmp, strlen(msg) + 1024, msg, args);
     va_end(args);
 
     msg_info = gtk_message_dialog_new(GTK_WINDOW(widget_get_widget_by_name("main_win")),
         GTK_DIALOG_DESTROY_WITH_PARENT,
         GTK_MESSAGE_INFO,
         GTK_BUTTONS_OK,
-        "%s", msg_tmp);
+        NULL);
 
     gtk_window_set_title(GTK_WINDOW(msg_info), "Information");
-        gtk_window_set_deletable(GTK_WINDOW(msg_info), false);
+    gtk_window_set_deletable(GTK_WINDOW(msg_info), false);
+    gtk_window_set_default_size(GTK_WINDOW(msg_info), 500, 100);
+
+    content = gtk_message_dialog_get_message_area(GTK_MESSAGE_DIALOG(msg_info));
+
+    if (!content) {
+        g_print("Failed to get info message box content!\n");
+
+        g_free((void *)msg_tmp);
+        return -1;
+    }
+
+    label = gtk_label_new(msg_tmp);
+
+    if (!label) {
+        g_print("Failed to create new info message box label!\n");
+
+        g_free((void *)msg_tmp);
+        return -1;
+    }
+
+    gtk_label_set_line_wrap(GTK_LABEL(label), true);
+    gtk_label_set_xalign(GTK_LABEL(label), 0);
+
+    gtk_widget_set_vexpand(label, true);
+    gtk_widget_set_hexpand(label, true);
+
+    gtk_container_add(GTK_CONTAINER(content), label);
+    gtk_widget_show_all(msg_info);
 
     gtk_dialog_run(GTK_DIALOG(msg_info));
 
@@ -51,7 +80,7 @@ gint msgbox_err(const gchar *msg, ...) {
     gint ret = 0;
     va_list args;
     gchar *msg_tmp = NULL;
-    GtkWidget *msg_err = NULL;
+    GtkWidget *msg_err = NULL, *content = NULL, *label = NULL;
 
 
 
@@ -61,7 +90,7 @@ gint msgbox_err(const gchar *msg, ...) {
         return -1;
     }
 
-    msg_tmp = g_malloc(strlen(msg) + 1);
+    msg_tmp = g_malloc(strlen(msg) + 1024);
 
     if (!msg_tmp) {
         g_print("Failed to allocate info message buffer!\n");
@@ -69,7 +98,7 @@ gint msgbox_err(const gchar *msg, ...) {
     }
 
     va_start(args, msg);
-    ret = g_vsnprintf(msg_tmp, strlen(msg) + 1, msg, args);
+    ret = g_vsnprintf(msg_tmp, strlen(msg) + 1024, msg, args);
     va_end(args);
 
     msg_err = gtk_message_dialog_new(GTK_WINDOW(widget_get_widget_by_name("main_win")),
@@ -80,6 +109,34 @@ gint msgbox_err(const gchar *msg, ...) {
 
     gtk_window_set_title(GTK_WINDOW(msg_err), "Error");
     gtk_window_set_deletable(GTK_WINDOW(msg_err), false);
+    gtk_window_set_default_size(GTK_WINDOW(msg_err), 500, 100);
+
+    content = gtk_message_dialog_get_message_area(GTK_MESSAGE_DIALOG(msg_err));
+
+    if (!content) {
+        g_print("Failed to get info message box content!\n");
+
+        g_free((void *)msg_tmp);
+        return -1;
+    }
+
+    label = gtk_label_new(msg_tmp);
+
+    if (!label) {
+        g_print("Failed to create new info message box label!\n");
+
+        g_free((void *)msg_tmp);
+        return -1;
+    }
+
+    gtk_label_set_line_wrap(GTK_LABEL(label), true);
+    gtk_label_set_xalign(GTK_LABEL(label), 0);
+
+    gtk_widget_set_vexpand(label, true);
+    gtk_widget_set_hexpand(label, true);
+
+    gtk_container_add(GTK_CONTAINER(content), label);
+    gtk_widget_show_all(msg_err);
 
     gtk_dialog_run(GTK_DIALOG(msg_err));
 
